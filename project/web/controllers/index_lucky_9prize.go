c950397d3ce1bb53e0d7f45a574980339b50ac01
azhuang321 <azhuang321@gmail.com>
2021-11-02 13:25:30 +0800

    32.抽奖实现

---
+++ project/web/controllers/index_lucky_9prize.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"lottery/conf"
+	"lottery/models"
+)
+
+func (c *IndexController) prize(prizeCode int, limitBlack bool) *models.ObjGiftPrize {
+	var prizeGift *models.ObjGiftPrize
+	giftList := c.ServiceGift.GetAllUse(false)
+	for _, gift := range giftList {
+		if gift.PrizeCodeA <= prizeCode && gift.PrizeCodeB >= prizeCode {
+			// 中奖编码满足条件,可以中奖
+			if !limitBlack || gift.Gtype < conf.GtypeGiftSmall {
+				prizeGift = &gift
+				break
+			}
+		}
+	}
+	return prizeGift
+}
